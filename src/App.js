8f5e4cda36c1bf52531918ed50548dd87345bce1
Nabeel Ramzeen <nabeelramzeen@NabeelRmz.local>
2019-03-11 00:52:11 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -75,3 +75,20 @@ let mapStateToProps = state => {
 let connectedApp = connect(mapStateToProps)(App)
 
 export default connectedApp;
+
+// Simulate a playoffs' output following the next conditions:
+// - Division East and West follow this structure
+// https://s3.amazonaws.com/cortex-cerebro/playoff_structure.jpg
+// - Each division have 8 teams
+// - Each team have 21 players
+// - Each player has a fixed success rating ranging from 0.15 to 1 (at random)
+// - Based on a team's players success rating precalculate the average success rating for each teams
+
+// - Based on the team success rating, calculate the tournament output on each division simulating
+//   !!! only 1 match per teams match-up !!!
+//   if Team 1 has a 0.56 rating and Team 2 has a 0.76 rating. Team 2 has 0.20 more chances to win the match.
+// - Based on the winning team of each division output the winner between the 2 division
+// - Use object-oriented programming
+
+// Evaluation criteria: Rapidity of execution, Algorithms' optimization
+// Use this page to complete the test
